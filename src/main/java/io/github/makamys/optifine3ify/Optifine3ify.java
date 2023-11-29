452fba7927b476a5a31463ff1409c7e6f4660315
makamys <makamys@outlook.com>
2023-11-29 14:33:53 +0100

    Init project

--- src/main/java/io/github/makamys/optifine3ify/Optifine3ify.java
+++ src/main/java/io/github/makamys/optifine3ify/Optifine3ify.java
@@ -1,14 +1,14 @@
-package ${package};
+package io.github.makamys.optifine3ify;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ${mainclass}.MODID, version = ${mainclass}.VERSION)
-public class ${mainclass}
+@Mod(modid = Optifine3ify.MODID, version = Optifine3ify.VERSION)
+public class Optifine3ify
 {
-    public static final String MODID = "${modid}";
+    public static final String MODID = "optifine3ify";
     public static final String VERSION = "@VERSION@";
 
     @EventHandler
